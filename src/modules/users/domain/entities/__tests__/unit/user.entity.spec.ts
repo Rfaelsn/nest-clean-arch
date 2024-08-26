58080a9251b843d83aac1bef414bbb4faa742964
Rfaelsn <rafaelnascimentovf6@gmail.com>
2024-08-26 16:20:45 -0300

    feat[userEntity]: criacao dos tests de unidade

---
+++ src/modules/users/domain/entities/__tests__/unit/user.entity.spec.ts
@@ -0,0 +1,23 @@
+import { faker } from '@faker-js/faker';
+import { UserEntity, UserProps } from '../../user.entity';
+
+describe('UserEntity unit tests', () => {
+  let sut: UserEntity;
+  let props: UserProps;
+
+  beforeEach(() => {
+    props = {
+      name: faker.person.fullName(),
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+    };
+    sut = new UserEntity({ ...props });
+  });
+
+  it('should create a new UserEntity', () => {
+    expect(sut.props.name).toEqual(props.name);
+    expect(sut.props.email).toEqual(props.email);
+    expect(sut.props.password).toEqual(props.password);
+    expect(sut.props.createdAt).toBeInstanceOf(Date);
+  });
+});
