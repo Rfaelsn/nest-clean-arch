7570cf2d3afc90418f6d6366a6c6cab23475971b
Rfaelsn <rafaelnascimentovf6@gmail.com>
2024-08-26 16:45:25 -0300

    feat[userEntity]: testando gets

--- src/modules/users/domain/entities/__tests__/unit/user.entity.spec.ts
+++ src/modules/users/domain/entities/__tests__/unit/user.entity.spec.ts
@@ -20,4 +20,27 @@ describe('UserEntity unit tests', () => {
     expect(sut.props.password).toEqual(props.password);
     expect(sut.props.createdAt).toBeInstanceOf(Date);
   });
+
+  it('should return the name when the getter is called', () => {
+    expect(sut.props.name).toBeDefined();
+    expect(sut.props.name).toEqual(props.name);
+    expect(typeof sut.props.name).toBe('string');
+  });
+
+  it('should return the email when the getter is called', () => {
+    expect(sut.props.email).toBeDefined();
+    expect(sut.props.email).toEqual(props.email);
+    expect(typeof sut.props.email).toBe('string');
+  });
+
+  it('should return the password when the getter is called', () => {
+    expect(sut.props.password).toBeDefined();
+    expect(sut.props.password).toEqual(props.password);
+    expect(typeof sut.props.password).toBe('string');
+  });
+
+  it('should return the createdAt when the getter is called', () => {
+    expect(sut.props.createdAt).toBeDefined();
+    expect(sut.props.createdAt).toBeInstanceOf(Date);
+  });
 });
