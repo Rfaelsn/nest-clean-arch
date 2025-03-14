c4b393b40cb5978b3d19332900683d9d1edba00c
Rfaelsn <rafaelnascimentovf6@gmail.com>
2025-03-14 11:05:33 -0300

    feat: create pagination presenter

--- src/modules/users/infrastructure/__tests__/unit/users.controller.spec.ts
+++ src/modules/users/infrastructure/__tests__/unit/users.controller.spec.ts
@@ -10,131 +10,142 @@ import { UpdatePasswordUseCase } from '@/modules/users/application/usecases/upda
 import { UpdatePasswordDto } from '../../dtos/update-password.dto';
 import { GetUserUseCase } from '@/modules/users/application/usecases/get-user.usecase';
 import { ListUsersUseCase } from '@/modules/users/application/usecases/list-users.usecase';
-import { SearchParams } from '@/shared/domain/repositories/searchable-repository-contract';
+import { UserPresenter } from '../../presenters/user.presenter';
 
 describe('UsersController', () => {
   let sut: UsersController;
   let id: string;
-  let props: UserOutputDto
+  let props: UserOutputDto;
 
   beforeEach(async () => {
-    sut = new UsersController()
-    id = 'a1442255-e2b0-47d0-ae53-57c0d0d2ba0b'
+    sut = new UsersController();
+    id = 'a1442255-e2b0-47d0-ae53-57c0d0d2ba0b';
     props = {
       id,
       name: 'Jhon Doe',
       email: 'a@a.com',
       password: '1234',
-      createdAt: new Date()
-    }
+      createdAt: new Date(),
+    };
   });
 
   it('should be defined', () => {
     expect(sut).toBeDefined();
   });
 
-  it('should create a user',async () => {
-    const output: SignupUseCase.Output = props
+  it('should create a user', async () => {
+    const output: SignupUseCase.Output = props;
     const mockSignupUseCase = {
-      execute: jest.fn().mockReturnValue(Promise.resolve(output))
-    }
-    sut['signupUseCase'] = mockSignupUseCase as any
+      execute: jest.fn().mockReturnValue(Promise.resolve(output)),
+    };
+    sut['signupUseCase'] = mockSignupUseCase as any;
     const input: SignupDto = {
       name: 'Jhon Doe',
       email: 'a@a.com',
       password: '1234',
-    }
-    const result = await sut.create(input)
-    expect(result).toMatchObject(output);
-    expect(mockSignupUseCase.execute).toHaveBeenCalledWith(input)
+    };
+    const presenter = await sut.create(input);
+    expect(presenter).toBeInstanceOf(UserPresenter);
+    expect(presenter).toStrictEqual(new UserPresenter(output));
+    expect(mockSignupUseCase.execute).toHaveBeenCalledWith(input);
   });
 
-  it('should authenticate a user',async () => {
-    const output: SigninUseCase.Output = props
+  it('should authenticate a user', async () => {
+    const output: SigninUseCase.Output = props;
     const mockSigninUseCase = {
-      execute: jest.fn().mockReturnValue(Promise.resolve(output))
-    }
-    sut['signinUseCase'] = mockSigninUseCase as any
+      execute: jest.fn().mockReturnValue(Promise.resolve(output)),
+    };
+    sut['signinUseCase'] = mockSigninUseCase as any;
     const input: SigninDto = {
       email: 'a@a.com',
       password: '1234',
-    }
-    const result = await sut.login(input)
-    expect(result).toMatchObject(output);
-    expect(mockSigninUseCase.execute).toHaveBeenCalledWith(input)
+    };
+    const presenter = await sut.login(input);
+    expect(presenter).toBeInstanceOf(UserPresenter);
+    expect(presenter).toMatchObject(new UserPresenter(output));
+    expect(mockSigninUseCase.execute).toHaveBeenCalledWith(input);
   });
 
-  it('should update a user',async () => {
-    const output: UpdateUserUseCase.Output = props
+  it('should update a user', async () => {
+    const output: UpdateUserUseCase.Output = props;
     const mockUpdateUserUseCase = {
-      execute: jest.fn().mockReturnValue(Promise.resolve(output))
-    }
-    sut['updateUserUseCase'] = mockUpdateUserUseCase as any
+      execute: jest.fn().mockReturnValue(Promise.resolve(output)),
+    };
+    sut['updateUserUseCase'] = mockUpdateUserUseCase as any;
     const input: UpdateUserDto = {
       name: 'new name',
-    }
-    const result = await sut.update(id,input)
-    expect(result).toMatchObject(output);
-    expect(mockUpdateUserUseCase.execute).toHaveBeenCalledWith({id,...input})
+    };
+    const presenter = await sut.update(id, input);
+    expect(presenter).toBeInstanceOf(UserPresenter);
+    expect(presenter).toMatchObject(new UserPresenter(output));
+    expect(mockUpdateUserUseCase.execute).toHaveBeenCalledWith({
+      id,
+      ...input,
+    });
   });
 
-  it('should update a users password',async () => {
-    const output: UpdatePasswordUseCase.Output = props
+  it('should update a users password', async () => {
+    const output: UpdatePasswordUseCase.Output = props;
     const mockUpdatePasswordUseCase = {
-      execute: jest.fn().mockReturnValue(Promise.resolve(output))
-    }
-    sut['updatePasswordUseCase'] = mockUpdatePasswordUseCase as any
+      execute: jest.fn().mockReturnValue(Promise.resolve(output)),
+    };
+    sut['updatePasswordUseCase'] = mockUpdatePasswordUseCase as any;
     const input: UpdatePasswordDto = {
       password: 'new password',
-      oldPassword:'old password'
-    }
-    const result = await sut.updatePassword(id,input)
-    expect(result).toMatchObject(output);
-    expect(mockUpdatePasswordUseCase.execute).toHaveBeenCalledWith({id,...input})
+      oldPassword: 'old password',
+    };
+    const presenter = await sut.updatePassword(id, input);
+    expect(presenter).toBeInstanceOf(UserPresenter);
+    expect(presenter).toMatchObject(new UserPresenter(output));
+    expect(mockUpdatePasswordUseCase.execute).toHaveBeenCalledWith({
+      id,
+      ...input,
+    });
   });
 
-  it('should delete a user',async () => {
-    const output = undefined
+  it('should delete a user', async () => {
+    const output = undefined;
     const mockDeleteUserUseCase = {
-      execute: jest.fn().mockReturnValue(Promise.resolve(output))
-    }
-    sut['deleteUserUseCase'] = mockDeleteUserUseCase as any
+      execute: jest.fn().mockReturnValue(Promise.resolve(output)),
+    };
+    sut['deleteUserUseCase'] = mockDeleteUserUseCase as any;
 
-    const result = await sut.remove(id)
+    const result = await sut.remove(id);
     expect(result).toStrictEqual(output);
-    expect(mockDeleteUserUseCase.execute).toHaveBeenCalledWith({id})
+    expect(mockDeleteUserUseCase.execute).toHaveBeenCalledWith({ id });
   });
 
-  it('should return a user',async () => {
+  it('should return a user', async () => {
     const output: GetUserUseCase.Output = props;
     const mockGetUserUseCase = {
-      execute: jest.fn().mockReturnValue(Promise.resolve(output))
-    }
-    sut['getUserUseCase'] = mockGetUserUseCase as any
+      execute: jest.fn().mockReturnValue(Promise.resolve(output)),
+    };
+    sut['getUserUseCase'] = mockGetUserUseCase as any;
 
-    const result = await sut.findOne(id)
-    expect(result).toStrictEqual(output);
-    expect(mockGetUserUseCase.execute).toHaveBeenCalledWith({id})
+    const presenter = await sut.findOne(id);
+    expect(presenter).toBeInstanceOf(UserPresenter);
+    expect(presenter).toStrictEqual(new UserPresenter(output));
+    expect(mockGetUserUseCase.execute).toHaveBeenCalledWith({ id });
   });
 
-  it('should list users',async () => {
+  it('should list users', async () => {
     const output: ListUsersUseCase.Output = {
       items: [props],
       currentPage: 1,
-      lastPage:1,
-      perPage:1,
-      total:1
-    }
+      lastPage: 1,
+      perPage: 1,
+      total: 1,
+    };
     const mockListUsersUseCase = {
-      execute: jest.fn().mockReturnValue(Promise.resolve(output))
-    }
-    sut['listUsersUseCase'] = mockListUsersUseCase as any
+      execute: jest.fn().mockReturnValue(Promise.resolve(output)),
+    };
+    sut['listUsersUseCase'] = mockListUsersUseCase as any;
     const searchParams = {
-      page:1,
-      perPage:1
-    }
-    const result = await sut.search(searchParams)
+      page: 1,
+      perPage: 1,
+    };
+    const result = await sut.search(searchParams);
     expect(result).toMatchObject(output);
-    expect(mockListUsersUseCase.execute).toHaveBeenCalledWith(searchParams)
+    expect(mockListUsersUseCase.execute).toHaveBeenCalledWith(searchParams);
   });
 });
---
+++ src/modules/users/infrastructure/presenters/__tests__/unit/user.presenter.spec.ts
@@ -0,0 +1,37 @@
+import { instanceToPlain } from 'class-transformer';
+import { UserPresenter } from '../../user.presenter';
+
+describe('UserPresenter', () => {
+  const createdAt = new Date();
+  const props = {
+    id: 'a1442255-e2b0-47d0-ae53-57c0d0d2ba0b',
+    name: 'Jhon Doe',
+    email: 'a@a.com',
+    password: 'fake',
+    createdAt,
+  };
+  let sut: UserPresenter;
+
+  beforeEach(() => {
+    sut = new UserPresenter(props);
+  });
+
+  describe('constructor', () => {
+    it('should be defined', () => {
+      expect(sut.id).toEqual(props.id);
+      expect(sut.name).toEqual(props.name);
+      expect(sut.email).toEqual(props.email);
+      expect(sut.createdAt).toEqual(props.createdAt);
+    });
+
+    it('should presenter data', () => {
+      const output = instanceToPlain(sut);
+      expect(output).toStrictEqual({
+        id: 'a1442255-e2b0-47d0-ae53-57c0d0d2ba0b',
+        name: 'Jhon Doe',
+        email: 'a@a.com',
+        createdAt: createdAt.toISOString(),
+      });
+    });
+  });
+});
